669518a01cf5e1790b2162b0e74bfc6fdba7d851
Noah Lundquist <nalundquist@gmail.com>
2022-08-19 08:40:25 -0700

    add second test, fulfill requirements

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 // Business Logic
 
 function roboger(numInput) {
-	const robogerInput = numInput;
-	return robogerInput
+	const robogerInput = parseInt(numInput);
+	const robogerError = "It is not a wonderful day in the neighborhood."
+	if (isNaN(robogerInput) || typeof robogerInput != 'number') {
+		return robogerError
+	} else {
+		return robogerInput
+	}
 }
