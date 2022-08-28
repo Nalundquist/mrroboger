0ddec49c64923b2b3a49b0ba5f440013f9eb5899
Noah Lundquist <nalundquist@gmail.com>
2022-08-28 16:34:15 -0700

    WIP: troubleshooting new structure with console logs

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function roboger(numInput) {
 	const robogerError = "It is not a wonderful day in the neighborhood.  Please supply me with a positive number.";
 	const robogerError2 = "That number...It is horrifying to me.  Please choose one smaller.  Under 5000, perhaps."
 	let robogerResult = "";
-	const robogerOutput = []
+	let robogerOutput = []
 	if (isNaN(robogerInput) || typeof robogerInput != 'number' || robogerInput < 0) {
 		robogerResult = robogerError;
 		return robogerResult;
@@ -14,34 +14,38 @@ function roboger(numInput) {
 		robogerResult = robogerError2;
 		return robogerResult;
 	} else {
+		console.log("inside final else after error checks")
 		for (i = 0; i <= robogerInput; i++) {
-			robogerOutput.push(i);
-			const robogerString = robogerOutput.map(function(num) {
-				return num.toString();
-			})
+			console.log("inside tostring for statement")
+			robogerOutput.push(i.toString());
+			// const robogerString = robogerOutput.map(function(num) {
+			// 	return num.toString();
+			// })
+			return robogerOutput
 		}
-	}
-	for (i = 0; i < robogerString.length; i++){
-		const robogerIndex = robogerString[i];
-		if (robogerIndex === " 143"){
-			robogerString[i] = ' "I love you."'
-		} else if (robogerIndex.includes(3)){
-			robogerString[i] = ' "Won\'t you be my neighbor?"';
-		} else if (robogerIndex.includes(2)){
-			robogerString[i] = ' "Boop!"';
-		} else if (robogerIndex.includes(1)){
-			robogerString[i] = ' "Beep!"';
-		} else {
-
+			for (i = 0; i < robogerOutput.length; i++){
+				const robogerIndex = robogerOutput[i];
+				console.log("inside for loop (start)");
+				if (robogerIndex === " 143"){
+					console.log("inside if loop 143");
+					robogerOutput[i] = ' "I love you."'
+				} else if (robogerIndex.includes(3)){
+					robogerOutput[i] = ' "Won\'t you be my neighbor?"';
+				} else if (robogerIndex.includes(2)){
+					robogerOutput[i] = ' "Boop!"';
+				} else if (robogerIndex.includes(1)){
+					robogerOutput[i] = ' "Beep!"';
+				} else {
+					for (i = 1; i < robogerOutput.length; i++) {
+						const robogerIndex = robogerOutput[i];
+						const preSpace = " ";
+						robogerOutput[i] = preSpace.concat(robogerIndex);
+				}
+			}
 		}
-	}
-	for (i = 1; i < robogerString.length; i++) {
-		const robogerIndex = robogerString[i];
-		const preSpace = " ";
-		robogerString[i] = preSpace.concat(robogerIndex);
 	}
 	if (robogerResult != robogerError && robogerResult != robogerError2){
-		robogerResult = robogerString.toString("");
+		robogerResult = robogerOutput.toString("");
 	}
 	return robogerResult;
 }
