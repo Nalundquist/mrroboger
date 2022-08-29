5c83ef2465484f5982a8c5e8a40ed736e50536b9
Noah Lundquist <nalundquist@gmail.com>
2022-08-28 17:25:22 -0700

    refactor code based upon feedback

--- js/scripts.js
+++ js/scripts.js
@@ -7,47 +7,35 @@ function roboger(numInput) {
 	const robogerError2 = "That number...It is horrifying to me.  Please choose one smaller.  Under 5000, perhaps."
 	let robogerResult = "";
 	let robogerOutput = []
-	if (isNaN(robogerInput) || typeof robogerInput != 'number' || robogerInput < 0) {
+	if (typeof robogerInput !== 'number' || robogerInput < 0) {
 		robogerResult = robogerError;
 		return robogerResult;
 	} else if (robogerInput >= 5000){
 		robogerResult = robogerError2;
 		return robogerResult;
 	} else {
-		console.log("inside final else after error checks")
 		for (i = 0; i <= robogerInput; i++) {
-			console.log("inside tostring for statement")
 			robogerOutput.push(i.toString());
-			// const robogerString = robogerOutput.map(function(num) {
-			// 	return num.toString();
-			// })
-			return robogerOutput
 		}
-			for (i = 0; i < robogerOutput.length; i++){
-				const robogerIndex = robogerOutput[i];
-				console.log("inside for loop (start)");
-				if (robogerIndex === " 143"){
-					console.log("inside if loop 143");
-					robogerOutput[i] = ' "I love you."'
-				} else if (robogerIndex.includes(3)){
-					robogerOutput[i] = ' "Won\'t you be my neighbor?"';
-				} else if (robogerIndex.includes(2)){
-					robogerOutput[i] = ' "Boop!"';
-				} else if (robogerIndex.includes(1)){
-					robogerOutput[i] = ' "Beep!"';
-				} else {
-					for (i = 1; i < robogerOutput.length; i++) {
-						const robogerIndex = robogerOutput[i];
-						const preSpace = " ";
-						robogerOutput[i] = preSpace.concat(robogerIndex);
-				}
+		for (i = 0; i < robogerOutput.length; i++){
+			if (robogerOutput[i] === "143"){
+				robogerOutput[i] = ' "I love you."'
+			} else if (robogerOutput[i].includes(3)){
+				robogerOutput[i] = ' "Won\'t you be my neighbor?"';
+			} else if (robogerOutput[i].includes(2)){
+				robogerOutput[i] = ' "Boop!"';
+			} else if (robogerOutput[i].includes(1)){
+				robogerOutput[i] = ' "Beep!"';
+			} else {
+				const preSpace = " ";
+				robogerOutput[i] = preSpace.concat(robogerOutput[i]);
 			}
 		}
 	}
 	if (robogerResult != robogerError && robogerResult != robogerError2){
 		robogerResult = robogerOutput.toString("");
+		return robogerResult;
 	}
-	return robogerResult;
 }
 
 // UI Logic
